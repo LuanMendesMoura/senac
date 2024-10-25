7f2ce6c9eddc67c51b069cbd984c9641d2575f45
LuanMendesMoura <luanmendesmoura091207@gmail.com>
2024-10-25 19:51:29 -0400

    JS curso cisco

---
+++ javaScript/cisco_js/main.js
@@ -0,0 +1,23 @@
+console.log("Hello, World!");
+console.log("Luan Mendes Moura");
+console.log("2007");
+console.log(2007);
+console.log("Luan","Mendes Moura","2007")
+console.log("Luan"+" Moura"+" 2007")
+
+let height = 180;
+let anotherHeight = height;
+console.log(height);
+console.log(anotherHeight);
+weight = 70
+console.log(weight);
+
+let greeting = "Hello!";
+let counter = 100;
+console.log(greeting);
+greeting = 1;
+console.log(greeting);
+
+greeting = "Hello!";
+greeting = greeting + counter;
+console.log(greeting);
